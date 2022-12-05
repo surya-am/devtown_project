70ad58b68420e6b0b6803c62abd1abbcc547468d
surya-am <115440468+surya-am@users.noreply.github.com>
2022-12-05 10:15:40 +0530

    Initial commit

---
+++ src/Components/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "./Header";
+import Notes from "./Notes";
+import Footer from "./Footer";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Notes />
+      <Footer />
+    </div>
+  );
+}
+export default App;
---
+++ src/Components/Notes.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+function Notes() {
+  return (
+    <div className="note">
+      <h1> Deep Learning </h1>
+      <p>
+        {" "}
+        Deep learning is a subset of machine learning, which is essentially a
+        neural network with three or more layers. These neural networks attempt
+        to simulate the behavior of the human brain—albeit far from matching its
+        ability—allowing it to “learn” from large amounts of data. While a
+        neural network with a single layer can still make approximate
+        predictions, additional hidden layers can help to optimize and refine
+        for accuracy.
+      </p>
+    </div>
+  );
+}
+export default Notes;
